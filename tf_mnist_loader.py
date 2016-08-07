2c0af0a87b8033df2158e2e72f6e6d0380866303
Zhongwen Xu <zhongwen.s.xu@gmail.com>
2016-08-07 20:16:20 +1000

    formatter

--- tf_mnist_loader.py
+++ tf_mnist_loader.py
@@ -22,6 +22,8 @@ import numpy
 from six.moves import urllib
 from six.moves import xrange  # pylint: disable=redefined-builtin
 SOURCE_URL = 'http://yann.lecun.com/exdb/mnist/'
+
+
 def maybe_download(filename, work_directory):
   """Download the data from Yann's website, unless it's already here."""
   if not os.path.exists(work_directory):
@@ -32,18 +34,21 @@ def maybe_download(filename, work_directory):
     statinfo = os.stat(filepath)
     print('Successfully downloaded', filename, statinfo.st_size, 'bytes.')
   return filepath
+
+
 def _read32(bytestream):
   dt = numpy.dtype(numpy.uint32).newbyteorder('>')
   return numpy.frombuffer(bytestream.read(4), dtype=dt)[0]
+
+
 def extract_images(filename):
   """Extract the images into a 4D uint8 numpy array [index, y, x, depth]."""
   print('Extracting', filename)
   with gzip.open(filename) as bytestream:
     magic = _read32(bytestream)
     if magic != 2051:
-      raise ValueError(
-          'Invalid magic number %d in MNIST image file: %s' %
-          (magic, filename))
+      raise ValueError('Invalid magic number %d in MNIST image file: %s' %
+                       (magic, filename))
     num_images = _read32(bytestream)
     rows = _read32(bytestream)
     cols = _read32(bytestream)
@@ -51,6 +56,8 @@ def extract_images(filename):
     data = numpy.frombuffer(buf, dtype=numpy.uint8)
     data = data.reshape(num_images, rows, cols, 1)
     return data
+
+
 def dense_to_one_hot(labels_dense, num_classes=10):
   """Convert class labels from scalars to one-hot vectors."""
   num_labels = labels_dense.shape[0]
@@ -58,22 +65,26 @@ def dense_to_one_hot(labels_dense, num_classes=10):
   labels_one_hot = numpy.zeros((num_labels, num_classes))
   labels_one_hot.flat[index_offset + labels_dense.ravel()] = 1
   return labels_one_hot
+
+
 def extract_labels(filename, one_hot=False):
   """Extract the labels into a 1D uint8 numpy array [index]."""
   print('Extracting', filename)
   with gzip.open(filename) as bytestream:
     magic = _read32(bytestream)
     if magic != 2049:
-      raise ValueError(
-          'Invalid magic number %d in MNIST label file: %s' %
-          (magic, filename))
+      raise ValueError('Invalid magic number %d in MNIST label file: %s' %
+                       (magic, filename))
     num_items = _read32(bytestream)
     buf = bytestream.read(num_items)
     labels = numpy.frombuffer(buf, dtype=numpy.uint8)
     if one_hot:
       return dense_to_one_hot(labels)
     return labels
+
+
 class DataSet(object):
+
   def __init__(self, images, labels, fake_data=False, one_hot=False):
     """Construct a DataSet. one_hot arg is used only if fake_data is true."""
     if fake_data:
@@ -81,8 +92,7 @@ class DataSet(object):
       self.one_hot = one_hot
     else:
       assert images.shape[0] == labels.shape[0], (
-          'images.shape: %s labels.shape: %s' % (images.shape,
-                                                 labels.shape))
+          'images.shape: %s labels.shape: %s' % (images.shape, labels.shape))
       self._num_examples = images.shape[0]
       # Convert shape from [num examples, rows, columns, depth]
       # to [num examples, rows*columns] (assuming depth == 1)
@@ -96,18 +106,23 @@ class DataSet(object):
     self._labels = labels
     self._epochs_completed = 0
     self._index_in_epoch = 0
+
   @property
   def images(self):
     return self._images
+
   @property
   def labels(self):
     return self._labels
+
   @property
   def num_examples(self):
     return self._num_examples
+
   @property
   def epochs_completed(self):
     return self._epochs_completed
+
   def next_batch(self, batch_size, fake_data=False):
     """Return the next `batch_size` examples from this data set."""
     if fake_data:
@@ -117,7 +132,8 @@ class DataSet(object):
       else:
         fake_label = 0
       return [fake_image for _ in xrange(batch_size)], [
-          fake_label for _ in xrange(batch_size)]
+          fake_label for _ in xrange(batch_size)
+      ]
     start = self._index_in_epoch
     self._index_in_epoch += batch_size
     if self._index_in_epoch > self._num_examples:
@@ -134,9 +150,13 @@ class DataSet(object):
       assert batch_size <= self._num_examples
     end = self._index_in_epoch
     return self._images[start:end], self._labels[start:end]
+
+
 def read_data_sets(train_dir, fake_data=False, one_hot=False):
+
   class DataSets(object):
     pass
+
   data_sets = DataSets()
   if fake_data:
     data_sets.train = DataSet([], [], fake_data=True, one_hot=one_hot)
